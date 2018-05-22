3398ae92fb2178bab4e845f84f258c33fd074a2e
Alen Hrga <ahrga93@gmail.com>
2018-05-22 17:57:28 +0200

    Change migrations to use UUID2 for PRIMARY KEY

--- src/main/resources/migrations/V201802281045__Initial_migration.sql
+++ src/main/resources/migrations/V201802281045__Initial_migration.sql
@@ -1,13 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS codeapp;
 
-
 DROP TABLE IF EXISTS configuration;
 DROP TABLE IF EXISTS log;
 DROP TABLE IF EXISTS social_connection;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id VARCHAR(255) DEFAULT gen_random_uuid(),
   first_name VARCHAR(500),
   last_name VARCHAR(500),
   email VARCHAR(100),
@@ -17,14 +16,15 @@ CREATE TABLE users (
   authority_group VARCHAR(200),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  created_by INTEGER,
-  modified_by INTEGER
+  created_by VARCHAR(255),
+  modified_by VARCHAR(255),
+  CONSTRAINT pk_user PRIMARY KEY (id)
 );
 
 CREATE TABLE social_connection (
   social_id   SERIAL PRIMARY KEY,
   social_type VARCHAR(100),
-  user_id SERIAL REFERENCES users (id)
+  user_id VARCHAR(255) REFERENCES users (id)
 );
 
 CREATE TABLE configuration (
@@ -34,15 +34,15 @@ CREATE TABLE configuration (
   conf_value  VARCHAR(255),
   created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  created_by  INTEGER,
-  modified_by INTEGER
+  created_by  VARCHAR(255),
+  modified_by VARCHAR(255)
 );
 
 CREATE TABLE log (
   id         SERIAL PRIMARY KEY ,
   action     VARCHAR(50),
   time       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  user_id    SERIAL,
+  user_id    VARCHAR (255),
   device     VARCHAR(512),
   is_success BOOLEAN,
   error      TEXT
